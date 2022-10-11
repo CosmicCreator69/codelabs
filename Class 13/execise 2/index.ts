d663d982f133b9a40fb7bbcbc79f7013cdc6baac
CosmicCreator69 <112199101+CosmicCreator69@users.noreply.github.com>
2022-10-11 14:16:23 -0500

    worked on new execises

---
+++ Class 13/execise 2/index.ts
@@ -0,0 +1,24 @@
+class logger{
+  
+   logRandomNumber() {
+Math.floor(Math.random()*101)
+console.log(this.logRandomNumber);
+
+   }
+   logARandomColor(){
+
+   }
+   logTo100() {
+
+   }
+
+}
+new logger{
+    
+}
+
+
+
+
+
+
